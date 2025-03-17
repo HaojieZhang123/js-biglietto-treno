01654f279abcf4a155c85e70f6f91cbdf035c79e
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-17 14:10:08 +0100

    [UPD] Created all needed variables

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Creazione variabili
+let chilometraggio = prompt("Quanti chilometri devi percorrere?");
+let eta = prompt("Quanti anni hai?");
+let prezzo = chilometraggio * 0.21;
+let prezzoFinale = prezzo;
