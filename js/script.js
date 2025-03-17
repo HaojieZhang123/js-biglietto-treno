c586c9784d98221a334bfbff6c437c35802f8802
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-17 14:11:11 +0100

    [UPD] Underage check

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ let chilometraggio = prompt("Quanti chilometri devi percorrere?");
 let eta = prompt("Quanti anni hai?");
 let prezzo = chilometraggio * 0.21;
 let prezzoFinale = prezzo;
+// check if underage
+if (eta < 18) {
+    prezzoFinale = prezzo * 0.8;
+}
